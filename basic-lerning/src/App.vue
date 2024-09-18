f2a4824fdb2ab83c54bb532d34eee8d905cbdc81
admin <admin@mail.com>
2024-09-18 11:54:27 +0800

    update again

--- basic-lerning/src/App.vue
+++ basic-lerning/src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <div>
-      <input type="text" placeholder="请输入待办事项点击回车将它添加到列表中...">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <div class="todoTitle">
+      <input class="addTodos" type="text" placeholder="请输入待办事项点击回车将它添加到列表中...">
+    </div>
+    <div class="TodoList">
+        <div class="TodoItem">
+          <input type="checkbox">打代码
+        </div>
     </div>
   </div>
 </template>
@@ -16,14 +21,34 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app{
-      width: 200px;
-      height: 200px;
-      background-color: #f0f0f0;
-      border: 1px solid #ccc;
+      width: 1000px;
+      height: 1000px;
+      background-color: #f4f7f4;
+      border: 1px solid #f1eaea;
       box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.3);
       margin: 50px auto;
+      padding: 50px;
+      .todoTitle{
+        padding: 0px;
+        .addTodos{
+            width: 100%;
+            height: 30px;
+            // margin: 30px 30px;
+            font-size: medium;
+            padding: 2px 15px;
+        }
+      }
+      
+      .TodoList{
+        width: 100%;
+        border: 1px solid #f1eaea;
+        .TodoItem{
+          width: 100%;
+          
+        }
+      }
 }
 
 
