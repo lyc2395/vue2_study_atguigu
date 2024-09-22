bdded168eda73ad75cccd7e7047ec62817b22fb5
Administrator <lyc2395@gamil.com>
2024-09-22 17:24:53 +0800

    css done for todolist

--- basic-lerning/src/App.vue
+++ basic-lerning/src/App.vue
@@ -12,23 +12,24 @@
               <input type="checkbox">
               <span>xxxx</span>
             </label>
-            <button class="btn btn-danger" style="display: none;">删除</button>
+            <button class="btn btn-danger">删除</button>
           </li>
           <li>
             <label>
               <input type="checkbox">
               <span>xxxx</span>
             </label>
+            <button class="btn btn-danger" >删除</button>
           </li>
-          <button class="btn btn-danger" style="display: none;">删除</button>
+          
         </ul>
         <div class="todo-footer">
           <label>
             <input type="checkbox">
+            <span>
+              <span>已完成0</span> / 全部2
+            </span>
           </label>
-          <span>
-            <span>已完成0</span> / 全部2
-          </span>
           <button  class="btn btn-danger">清除已完成任务</button>
         </div>
       </div>
@@ -48,7 +49,7 @@ export default {
 
 <style lang="scss">
 .todo-container{
-      width: 1000px;
+      width: 80%;
       height: 1000px;
       background-color: #f4f7f4;
       border: 1px solid #f1eaea;
@@ -69,17 +70,73 @@ export default {
       
       .todo-list{
         width: 100%;
-        border: 1px solid rgba(0, 0, 0, 0.742);
+        
         margin: 20px 0px;
         padding: 0px;
         li{
-          margin: 10px;
+          display: flex;
+          justify-content: space-between;
+          border: 1px solid rgba(0, 0, 0, 0.742);
+          margin-bottom: -1px;
+          padding: 10px;
+          flex-wrap: nowrap;
           label{
+            display: flex;
             width: 100%;
-            box-sizing: border-box;
+            align-items: center;
+            gap: 10px;
+            span{
+              font-size: medium;
+            }
+          }
+          .btn {
+            white-space: nowrap;
+            display: none;
+            background-color: rgba(255, 0, 0, 0.966);
+            font-family: 'Courier New', Courier, monospace;
+            font-size: medium;
+            color: #f1eaea;
+            border: 1px solid rgba(255, 0, 0, 0.966) ;
+            padding: 5px;
+            font-weight: bold;
+          }
         }
+        li:hover .btn{
+          display: block;
+        }
+        li:hover{
+          background-color: rgb(81, 204, 241);
         }
         
+      }
+      .todo-footer{
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        padding: 10px;
+        box-sizing: border-box;
+        // align-items: center
+        label {
+          width: 10%;
+          display: flex;
+          align-items: center;
+          span{
+            margin-left: 10px;
+            font-size: medium;
+          }
+        }
+        button {
+          white-space: nowrap;
+          background-color: rgba(255, 0, 0, 0.966);
+          font-size: medium;
+          color: #f1eaea;
+          border: 1px solid rgba(255, 0, 0, 0.966) ;
+          padding: 5px;
+          font-weight: bold;
+        }
+      }
+      .todo-footer:hover {
+        background-color: skyblue;
       }
 }
 
